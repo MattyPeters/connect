aaf179ff9d1f2d972ad09009e8d7b304b7f18f57
MattyPeters <s3200344@student.rmit.edu.au>
2013-08-14 19:22:48 +1000

    Create form.php

---
+++ form.php
@@ -0,0 +1,49 @@
+
+
+<form action = "#" method= "GET">
+<br> Enter a Wine Name <br>
+<input type="text" name="WineName" Value = "All">
+<br> Enter a Winery Name <br>
+<input type="text" name="WineryName" Value = "All">
+<br> Choose Wine Region <br>
+<select name="WineRegion">
+         <?php
+		while($row = mysql_fetch_row($result)) {
+			$tableName = $row[0];
+			echo '<option value="$tableName">$tableName</option>';
+			}
+		 ?>
+         </select>
+<br> Choose Grape Variety <br>         
+<select name="Grape_Type">
+           <option value="?">?
+           <option value="?2">?2
+           <option value="?3">?3
+         </select>
+<br> The range of years <br>         
+<select name="LowerYears">
+           <option value="?">?l
+           <option value="?2">?2l
+           <option value="?3">?3l
+         </select>
+<select name="Upperyears">
+           <option value="?u">?u
+           <option value="?2u">?2u
+           <option value="?3u">?3u
+         </select>
+         
+<br> Enter Min Amount of Wines <br>
+<input type="text" name="Minqty" Value = "2">
+
+<br> Enter Max Amount of Wines <br>
+<input type="text" name="Maxqty" Value = "5">
+
+<br> Enter Min Cost of Wines <br>
+<input type="text" name="Mincost" Value = "10">
+
+<br> Enter Max Cost of Wines <br>
+<input type="text" name="Maxcost" Value = "100">
+<br>
+<br>
+<input type="submit" value="Search">
+
