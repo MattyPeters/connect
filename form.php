1a91f60fc88674cd84ee63fe09b202acdbee87a1
MattyPeters <s3200344@student.rmit.edu.au>
2013-08-14 20:08:21 +1000

    Update form.php

--- form.php
+++ form.php
@@ -1,31 +1,27 @@
 <html>
 <head>
-
-</head>
-
-<body>
-
-<php
-
+<?php
 require_once('db.php');
-if($dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
+if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW)) {
 echo 'Could not connect to mysql on ' . DB_HOST . '\n';
 exit;
 }
-
-if(mysql_select_db(DB_NAME, $dbconn)) {
+echo 'Connected to mysql <br />';
+if(!mysql_select_db(DB_NAME, $dbconn)) {
 echo 'Could not user database ' . DB_NAME . '\n';
 echo mysql_error() . '\n';
 exit;
 }
-
-mysql_select_db("winestore");
-
-echo "help!";
-
-
-
+echo 'Connected to database ' . DB_NAME . '\n';
 ?>
+</head>
+
+<body>
+
+
+
+
+
 
 
 <form action = "#" method= "GET">
