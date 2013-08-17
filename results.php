d125a0270c0ecb4ac337b8ac88241e9e5acea2fb
MattyPeters <s3200344@student.rmit.edu.au>
2013-08-17 16:40:31 +1000

    Update results.php

--- results.php
+++ results.php
@@ -1,7 +1,55 @@
 <html>
 <head>
 
+<php
+function showresults {
 
+$query = "SELECT wine_name, variety, year, winery_name, region_name, cost, on_hand
+FROM winery, region, wine, grape_variety, inventory, wine_variety
+WHERE winery.region_id = region.region_id
+AND wine.winery_id = winery.winery_id"
+AND wine.wine_id = inventory.wine_id"
+AND wine.wine_id = wine_variety.wine_id"
+AND grape_variety.variety_id = wine_variety.variety_id";
+
+$result = mysql_query($query);
+
+ $rowsFound = @ mysql_num_rows($result);
+
+    // If the query has results ...
+    
+    $rowsFound = @ mysql_num_rows($result);
+    if ($rowsFound > 0) {
+      // ... print out a header
+      print "Wines of $regionName<br>";
+
+      // and start a <table>.
+      print "\n<table>\n<tr>" .
+          "\n\t<th>Wine Name</th>" .
+          "\n\t<th>Variety</th>" .
+          "\n\t<th>Year</th>" .
+          "\n\t<th>Winery</th>" .
+          "\n\t<th>Region</th>" .
+          "\n\t<th>Cost</th>" .
+          "\n\t<th>Avail</th>\n</tr>";
+          
+          while ($row = @ mysql_fetch_array($result)) {
+        // Print one row of results
+        print "\n<tr>\n\t<td>{$row["wine_name"]}</td>" .
+            "\n\t<td>{$row["variety"]}</td>" .
+            "\n\t<td>{$row["year"]}</td>" .
+            "\n\t<td>{$row["winery_name"]}</td>" .
+               "\n\t<td>{$row["region_name"]}</td>" .
+                  "\n\t<td>{$row["cost"]}</td>" .
+            "\n\t<td>{$row["on_hand"]}</td>\n</tr>";
+      } // end while loop body
+
+      // Finish the <table>
+      print "\n</table>";
+    } // end if $rowsFound body
+
+}
+?>
 </head>
 <body>
 <?php
@@ -17,13 +65,13 @@ $maxqty = $_GET['Maxqty'];
 $mincost = $_GET['Mincost'];
 $maxcost = $_GET['Maxcost'];
 
-echo $winename.$winery.$region.$lowyear.$minqty;
+
+showresults();
 
 ?>
 
+
+
+
 </body>
-
 </html>
-
-
-
